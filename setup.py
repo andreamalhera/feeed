2b4d0dcda61e7b6f0264b3aec5301da3031cf7d5
Andrea Maldonado <>
2026-07-06 20:21:46 +0200

    Introduces feeed 2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-version_string = os.environ.get("VERSION_PLACEHOLDER", "1.9.0")
+version_string = os.environ.get("VERSION_PLACEHOLDER", "2.0.0")
 print(version_string)
 version = version_string
 
